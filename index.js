fd9ecb5dc290b8b22b10e46a589118a96291f91f
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-05 16:13:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,14 +18,6 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   
-  if(cmd === `${prefix}ciekawostka`){
-    return message.channel.send("Jeżeli wykopiemy dziure do środka ziemi i rzucimy ksążkę, to będzie ona spadać 45 minut", "Czy wiesz, że w ludzkim ciele jest więcej komórek bakterii niż komórek ciała?")
-  }
-
-  if(cmd === `${prefix}frost`){
-    return message.channel.send("https://cdn.discordapp.com/attachments/473083116731498501/475008357908873228/unknown.png")
-  }
-
   if(message.content === "!"){
     return message.channel.send("Nie drzyj mordy ludzie śpią!")
   }
