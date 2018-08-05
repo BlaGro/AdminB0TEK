7ec1b395da6f2080abc70bb1e4c51c8f44becb7e
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-05 19:37:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = ab!;
+  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
