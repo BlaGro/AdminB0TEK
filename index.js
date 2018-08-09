d06cf738afd8e91745e0e125f97aeffcd9b7f06c
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-09 15:39:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ bot.on("message", async message => {
   if(cmd === `${prefix}nazwaKomendy`){}
 
   if(message.content === "Cześć"){
-    message.react("✔")
-    return message.channel.send("Witaj", message.author.username)
+    message.react("🇼").then(message.react("🇮").then(message.react("🇹").then(message.react("🇦").then(message.react("🇯")))))
   }
 
   if(message.content === "Potrzebuje pomocy"){
