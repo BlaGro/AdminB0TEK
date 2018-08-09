2341f597bbdd6c3362181dd62ee4d521f366841b
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-09 19:53:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  let commandfile = bot.commands.get(cmd.slice(prefix.lenght));
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
   
   if(message.content === "Głosowanie"){
