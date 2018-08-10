82362208ab4db734f755646eabd30591414d5818
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-10 10:27:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ fs.readdir("./commands/", (err, files) => {
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
     console.log(`Załadowano ${f}`);
-    bot.comamnds.set(props.help.name, props)
+    bot.comamnds.get(props.help.name, props)
   });
 
 });
