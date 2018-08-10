7ac0e914ba99d52f812579f18518b3109b0f6cef
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-10 11:14:32 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,25 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args, tools) => {
+module.exports.run = async(bot, message, args) => {
 
-    if(!message.member.roles.find(r => r.name === 'Moderator')) return message.channel.send('Ta komenda wymaga rangi: Moderator');
+  if (!message.member.roles.find(r => r.name === "Administracja")) return message.channel.send("Nie posiadasz roli: Administracja");
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Nie masz permisji do użycia komendy ``głosowanie``");
 
-    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Ta komenda wymaga permissi: Zarządzanie wiadomościami');
+  if (!args[0]) return message.channel.send("Prawidłowe użycie to: <głosowanie [pytanie]");
 
-    if(!args[0]) return message.channel.send('Poprawne użycie: ab!poll <pytanie>');
+  const embed = new Discord.RichEmbed()
+    .setColor(0xffffff)
+    .setTitle("Głosowanie rozpoczęte!")
+    .setDescription(`**${args.join(" ")}**`)
+    .setFooter(`Głosowanie utworzone przez: ${message.author.username}`, `${message.author.avatarURL}`);
 
-    const embed = new Discord.MessageEmbed()
-    .setColor('#9842f4')
-    .setFooter('Kliknij reakcje aby zagłosować')
-    .setDescription(args.join(' '))
-    .setTitle('Stworzone przez ${message.author.username}');
+  let msg = await message.channel.send(embed);
 
-    let msg = await message.channel.send(embed);
-    await msg.react('✅')
-    await msg.react('❌')
-    message.delete({timeout: 1000});
+  await msg.react("✅");
+  await msg.react("❎");
+
+  message.delete({timeout: 1000});
 };
-
 module.exports.help = {
   name: "poll"
 };
