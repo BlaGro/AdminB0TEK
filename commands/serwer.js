d65041059a498e515a55fedf167b42d1005a2dc7
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-13 12:41:45 +0200

    Update serwer.js

--- commands/serwer.js
+++ commands/serwer.js
@@ -4,7 +4,6 @@ module.exports.run = async(bot, message, args) => {
 
   let embed = new Discord.RichEmbed()
   let ikonka = message.guild.iconURL;
-
   .setDescription("Info o serwerze")
   .setColor("#4286f4")
   .setThumbnail(ikonka)
