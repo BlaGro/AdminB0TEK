73f6ceac673068f10e52f4c2d49a9820ef78e181
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-13 14:05:26 +0200

    Create bot.js

---
+++ commands/bot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+  let bicon = bot.user.displayAvatarURL;
+  let embed = new Discord.RichEmbed()
+  .setDescription("Info o bocie")
+  .setColor("#0061ff")
+  .setThumbnail(bicon)
+  .addField("Nazwa bot", bot.user.username)
+  .addField("Stworzony", bot.user.createdAt)
+  .addField("ID bota", bot.user.id)
+  .addField("TAG bota", bot.user.tag)
+  .addField("Ostania wiadomość", bot.user.lastMessage || "Brak")
+  message.channel.send(embed);
+}
+
+module.exports.help = {
+  name:"bot"
+}
