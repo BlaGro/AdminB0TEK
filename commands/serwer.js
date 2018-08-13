5c69fbc60130270ea29e3dbd55e847bbafe95826
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-13 12:44:27 +0200

    Update serwer.js

--- commands/serwer.js
+++ commands/serwer.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-
-  let embed = new Discord.RichEmbed()
-  let ikonka = message.guild.iconURL;
-  .setDescription("Info o serwerze")
-  .setColor("#4286f4")
-  .setThumbnail(ikonka)
-  .addField("Właściciel", message.guild.owner)
-  .addField("Ilość użytkowników", message.guild.memberCount)
-  .addField("Jesteś na V", message.guild.name)
-  .addField("Region", message.guild.region)
-  .addField("Lewel weryfikacji", message.guild.verificationLevel)
-  .addField("Kanał AFK", message.guild.afkChannel || "Brak")
-  .addField("Rola domyślna", message.guild.deafaultRole)
-  .addField("Domyślny kanał", message.guild.deafaultChannel);
+    let embed = new Discord.RichEmbed()
+    let ikonka = message.guild.iconURL;
+    .setDescription("Info o serwerze")
+    .setColor("#4286f4")
+    .setThumbnail(ikonka)
+    .addField("Właściciel", message.guild.owner)
+    .addField("Ilość użytkowników", message.guild.memberCount)
+    .addField("Jesteś na V", message.guild.name)
+    .addField("Region", message.guild.region)
+    .addField("Lewel weryfikacji", message.guild.verificationLevel)
+    .addField("Kanał AFK", message.guild.afkChannel || "Brak")
+    .addField("Rola domyślna", message.guild.deafaultRole)
+    .addField("Domyślny kanał", message.guild.deafaultChannel);
 }
+
 module.exports.help = {
   name: "serwer"
 }
