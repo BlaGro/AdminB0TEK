c6973ad979d61c740025a6b23199ac71b6fa8205
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-19 12:58:30 +0200

    Update ustawienia.js

--- commands/ustawienia.js
+++ commands/ustawienia.js
@@ -1,7 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  message.channel.send("Na obeczny czas nie ma żadnych ustawień ;3")
+  let bot = bot.user.avatarURL;
+  let embed = new Discord.RichEmbed()
+  .setDescription("Ustawienia")
+  .setColor("#042966")
+  .setThumbnail(bot)
+  .addField("Komendy ustawień V", "Pewnie są jakieś (^.^)")
+  .addField("ab!timer <tekst>", "Ustaw timer z tekstem jaki ma pisać bot (pisze co 5 minut :)")
 }
 module.exports.help = {
   name:"ustawienia"
