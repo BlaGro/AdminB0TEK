2be923efc98575cb50a20e04b2510f8d5b0d633a
djsklfhfhsjfhsji <41817988+BlaGro@users.noreply.github.com>
2018-08-19 12:47:09 +0200

    Create ustawienia.js

---
+++ commands/ustawienia.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+  message.channel.send("Na obeczny czas nie ma żadnych ustawień ;3")
+}
+module.exports.help = {
+  name:"ustawienia"
+}
