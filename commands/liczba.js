4eeac279270c8155c554b1eddd7fd81ab69b5dea
anime <41817988+BlaGro@users.noreply.github.com>
2019-01-21 22:28:21 +0100

    Update liczba.js

--- commands/liczba.js
+++ commands/liczba.js
@@ -4,7 +4,7 @@ module.exports.run = async(bot, message, args) =>{
   var odpowiedzi = ["1","2","3","4","5","6","7","8","9","10"]
   var opcje = odpowiedzi[Math.floor(Math.random()*odpowiedzi.length)];
   let embed = new Discord.RichEmbed()
-  .addField("wylosowana liczba", `${opcje}`)
+  .addField("Wylosowana liczba", `${opcje}`)
   message.channel.send(embed);
 }
 
